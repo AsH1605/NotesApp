8fe215990bc7b45b7359d94f5b4cb9c39d366665
AsH1605 <akshitatiwary16@gmail.com>
2023-12-30 16:19:19 +0530

    Dependency issue solved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,8 +76,8 @@ dependencies {
     val room_version = "2.6.1"
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("androidx.room:room-runtime:$room_version")
-    annotationProcessor("androidx.room:room-compiler:$room_version")
-
+    kapt("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:2.6.1")
 }
 
 kapt {
